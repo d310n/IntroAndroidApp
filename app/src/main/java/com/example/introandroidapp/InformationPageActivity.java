0b8c378809cb424d49c1ad6c3b2aa3004b8d36d8
d310n <deiongarcia@gmail.com>
2019-08-17 10:28:39 -0500

    Added new code

--- app/src/main/java/com/example/introandroidapp/InformationPageActivity.java
+++ app/src/main/java/com/example/introandroidapp/InformationPageActivity.java
@@ -1,5 +1,6 @@
 package com.example.introandroidapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -37,11 +38,46 @@ public class InformationPageActivity extends AppCompatActivity implements View.O
                         .setAction("Action", null).show();
             }
         });
+
+        spnSeason = (Spinner) findViewById(R.id.spnSeason);
+        skbTemp = (SeekBar) findViewById(R.id.skbTemp);
+        swchAllergy = (Switch) findViewById(R.id.swchAllergy);
+        lblSeekValue = (TextView) findViewById(R.id.lblSeekValue);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(this);
+        skbTemp.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                String display = String.valueOf(progress);
+                lblSeekValue.setText(display);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekbar){
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     @Override
     public void onClick(View v){
-
+        String season, allergies;
+        int temperature;
+        season = spnSeason.getSelectedItem().toString();
+        temperature=skbTemp.getProgress();
+        allergies = (String) (swchAllergy.isChecked() ? swchAllergy
+                    .getTextOn() : swchAllergy.getTextOff()); //if the switch is on then the user has allergies
+        Intent intent = new Intent(InformationPageActivity.this, InformationResultsActivity.class); //pass information to the results activity
+        intent.putExtra("season", season);
+        intent.putExtra("temperature", temperature);
+        intent.putExtra("allergies", allergies);
+        this.startActivity(intent);
     }
 
 }
